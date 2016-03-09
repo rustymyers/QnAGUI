4bdc70ed34e69c3a8edeef837598eedf05ceec22
Rusty Myers <rzm102@74.64-26.108.118.128.in-addr.arpa>
2016-03-09 13:41:14 -0500

    Initial Commit.

---
+++ QnA/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  QnA
+//
+//  Created by Rusty Myers on 3/9/16.
+//  Copyright © 2016 Rusty Myers. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+        NSLog("Starting App")
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ QnA/ViewController.swift
@@ -0,0 +1,90 @@
+//
+//  ViewController.swift
+//  QnA
+//
+//  Created by Rusty Myers on 3/9/16.
+//  Copyright © 2016 Rusty Myers. All rights reserved.
+//
+// Icons generated from BigFix Logo using https://makeappicon.com/
+
+import Cocoa
+import Foundation
+
+class ViewController: NSViewController {
+    var qnaPath = "/Library/BESAgent/BESAgent.app/Contents/MacOS/QnA"
+    
+    @IBOutlet weak var queryBar: NSTextField!
+    @IBOutlet var queryOut: NSTextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        queryOut.editable = false
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+    @IBAction func queryButton(sender: AnyObject) {
+        // NSLog("We've hit a button!")
+        if queryBar.stringValue == "" {
+            NSLog("Blank String")
+            return
+        }
+        
+        // Get unquoted string from text bar
+        guard let newQuery:String = queryBar.stringValue else {
+            return
+        }
+        // Log new string
+        NSLog("Sending query: %@", newQuery)
+        
+        // Wrap in single quotes
+        let newCommand = ("\'" + newQuery + "\'" )
+        // Log new string
+        NSLog("New Command: %@", newCommand)
+        
+        // Add query to textbox
+        setqueryOutput("Q: " + newQuery + "\n")
+        
+        // Send query to shell and get output
+        let (shelloutput, _) = shell("/bin/bash", "-c", "echo " + newCommand + " | " + qnaPath)
+        // Set text view to output
+        setqueryOutput(shelloutput)
+        
+        NSLog("output: %@", shelloutput)
+
+    }
+
+    func shell(args: String...) -> (String, Int32) {
+        let task = NSTask()
+        let pipe = NSPipe()
+        
+        NSLog("%@", args)
+        
+        task.launchPath = args[0]
+        task.arguments = Array(args[1..<args.count])
+        task.standardOutput = pipe
+        
+        task.launch()
+        task.waitUntilExit()
+        
+        let outputdata = pipe.fileHandleForReading.readDataToEndOfFile()
+        let standardout = NSString(data: outputdata, encoding: NSUTF8StringEncoding)
+        
+        //return (standardout as! String)
+        return (standardout as! String, task.terminationStatus)
+    }
+    
+    func setqueryOutput(text: String = "") {
+        queryOut.textStorage?.mutableString.appendString(text)
+        queryOut.scrollToEndOfDocument(self)
+    }
+
+    
+}
+
